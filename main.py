653bd83fdb99b08961298cb46a7619976ade7fae
Tobias <tobias@habacker.de>
2026-09-04 18:49:01 +0200

    Use 9090 for Spotify OAuth callback

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 
-REDIRECT_URI = "http://127.0.0.1:8000/callback"
+REDIRECT_URI = "http://127.0.0.1:9090/callback"
 SCOPE = "playlist-read-private"
 
 
